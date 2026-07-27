0878d0afb3700959b9ee2669804532f55ed5422a
Ivan Suzikov <vankos7772@gmail.com>
2026-07-27 21:26:13 +0200

    Added Custom PlayerState

---
+++ Source/Aura/Public/Player/AuraPlayerState.h
@@ -0,0 +1,18 @@
+// Copyright Do Not Tell Dev
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "AuraPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+public:
+	AAuraPlayerState();
+};
